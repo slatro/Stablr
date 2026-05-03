afa2e2a727f67ac624c6126d411353960ffb1fca
slatro <slatro.eth@gmail.com>
2026-05-04 01:35:52 +0300

    UI: Perfectly recalibrate TradingView height (490px) to match Swap interface symmetry

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -110,7 +110,7 @@ export default function App() {
 
           {/* Chart Column (Order-2 on mobile, Order-1 on desktop) */}
           <div className="flex flex-col gap-4 order-2 xl:order-1">
-            <div className="glass-frame h-[450px] xl:h-[450px]">
+            <div className="glass-frame h-[490px] xl:h-[490px]">
               <PriceChart />
             </div>
             {/* Restored Chart Detail Bar */}
